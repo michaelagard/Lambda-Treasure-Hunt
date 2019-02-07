b8dba2ff95d6bd4a233e11c89d3cd57a91193614
michaelagard <mdagard@gmail.com>
2019-02-07 15:50:00 -0500

    import provider store middleware thunk logger rootreducer

--- lambda-treasure-hunt-react-client/src/index.js
+++ lambda-treasure-hunt-react-client/src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './reset.css';
-import App from './components/App/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./reset.css";
+import App from "./components/App/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import rootReducer from "./reducers";
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
