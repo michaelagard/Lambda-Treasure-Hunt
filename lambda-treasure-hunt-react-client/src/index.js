2c05900d71fdb9a76739b0f2f9833b85b8e71708
michaelagard <mdagard@gmail.com>
2019-02-08 12:14:32 -0500

    reformated index to be more readable

--- lambda-treasure-hunt-react-client/src/index.js
+++ lambda-treasure-hunt-react-client/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./reset.css";
+
 import App from "./components/App/App";
-import { Provider } from "react-redux";
+
 import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./reducers";
+
+import "./reset.css";
+
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
