429b149bcca3547ee6782e5ec1f0d12f88344b79
michaelagard <mdagard@gmail.com>
2019-02-08 16:59:31 -0500

    add conditional viewing of error and items

--- lambda-treasure-hunt-react-client/src/components/Sidebar/Sidebar.js
+++ lambda-treasure-hunt-react-client/src/components/Sidebar/Sidebar.js
@@ -25,16 +25,16 @@ const Sidebar = props => {
       <ul>
         <li>Cooldown: {props.room.cooldown}</li>
         <li>Coordinates: {props.room.coordinates}</li>
-        <li>Description: {props.room.description}</li>
+        {/* <li>Description: {props.room.description}</li> */}
         <li>Elevation: {props.room.elevation}</li>
-        <li>Errors: {props.room.errors}</li>
         <li>Exits: {props.room.exits}</li>
-        <li>Items: {props.room.items}</li>
+        {props.room.items === "" ? <li>Items: {props.room.items}</li> : ""}
         <li>Messages: {props.room.messages}</li>
         <li>Players: {props.room.players}</li>
         <li>Room ID: {props.room.room_id}</li>
         <li>Terrain: {props.room.terrain}</li>
         <li>Title: {props.room.title}</li>
+        {props.room.errors === "" ? <li>Errors: {props.room.errors}</li> : ""}
       </ul>
     </SidebarWrapper>
   );
