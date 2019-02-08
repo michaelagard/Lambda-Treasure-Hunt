bf612425dedea04faa035cb0fb18fb1cf04de982
michaelagard <mdagard@gmail.com>
2019-02-08 12:15:00 -0500

    add playerMove action

--- lambda-treasure-hunt-react-client/src/actions/index.js
+++ lambda-treasure-hunt-react-client/src/actions/index.js
@@ -12,6 +12,10 @@ export const FETCHING_INIT = "FETCHING_INIT";
 export const FETCHED_INIT = "FETCHED_INIT";
 export const FETCH_INIT_ERROR = "FETCH_INIT_ERROR";
 
+export const MOVING_PLAYER = "MOVING_PLAYER";
+export const MOVED_PLAYER = "MOVED_PLAYER";
+export const MOVE_PLAYER_ERROR = "MOVE_PLAYER_ERROR";
+
 export const checkStatus = () => {
   return dispatch => {
     dispatch({ type: FETCHING_STATUS });
@@ -19,7 +23,6 @@ export const checkStatus = () => {
       .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/status/")
 
       .then(response => {
-        console.log(response.data);
         dispatch({ type: FETCHED_STATUS, payload: response.data });
       })
 
@@ -27,17 +30,33 @@ export const checkStatus = () => {
   };
 };
 
-export const playerInitialization = () => {
+// export const playerInitialization = () => {
+//   return dispatch => {
+//     dispatch({ type: FETCHING_INIT });
+//     axios
+//       .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/init/")
+
+//       .then(response => {
+//         dispatch({ type: FETCHED_INIT, payload: response.data });
+//       })
+
+//       .catch(error => dispatch({ type: FETCH_INIT_ERROR, error: error }));
+//   };
+// };
+
+export const playerMove = direction => {
   return dispatch => {
-    dispatch({ type: FETCHING_INIT });
+    dispatch({ type: MOVING_PLAYER });
     axios
-      .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/init/")
+      .post(
+        `https://lambda-treasure-hunt.herokuapp.com/api/adv/move/`,
+        direction
+      )
 
       .then(response => {
-        console.log(response.data);
-        dispatch({ type: FETCHED_INIT, payload: response.data });
+        dispatch({ type: MOVED_PLAYER, payload: response.data });
       })
 
-      .catch(error => dispatch({ type: FETCH_INIT_ERROR, error: error }));
+      .catch(error => dispatch({ type: MOVE_PLAYER_ERROR, error: error }));
   };
 };
