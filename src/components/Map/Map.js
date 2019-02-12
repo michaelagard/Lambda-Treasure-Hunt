ab63adeb17c134727c680f728aca8f4ff9dba63a
michaelagard <mdagard@gmail.com>
2019-02-12 15:05:18 -0500

    add room style

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
-
+import styled from "styled-components";
+const Room = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 1px solid black;
+  background: grey;
+`;
 class Map extends Component {
   render() {
-    return <div className="Map" />;
+    return (
+      <div className="Map">
+        test
+        <Room />
+      </div>
+    );
   }
 }
 
