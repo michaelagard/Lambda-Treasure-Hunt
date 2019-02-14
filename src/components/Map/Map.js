11d5b901ca9de125e6796617f240a2f433960891
michaelagard <mdagard@gmail.com>
2019-02-14 18:44:29 -0500

    generate basic map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,28 +1,48 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+
 const Room = styled.div`
+  position: absolute;
   width: 20px;
   height: 20px;
-  border: 1px solid black;
+  left: ${props => props.inputLeft || "30px"};
+  top: ${props => props.inputTop || "30px"};
   background: grey;
 `;
 class Map extends Component {
   /* Map Generation
-  Step 1:
-  Get the max x and max y coordinate
-  Get the min x and min y coordinate
-  Find the difference between the x's and y's
-  
+  1. Get the max x and max y coordinate
+  for roomId in roomGraph:
+    coordinates = roomGraph[roomId][0]
+    exits = roomGraph[roomId][1]
+  2. Get the min x and min y coordinate
+  3. Find the difference between the x's and y's
+  4. plot out matrix of <Room/>s with the dimension of x and y from step 3.
+  5. add classes to each room starting with the smallest x and y and increment x from left to right and y to top to bottom
   */
-  generateGraph = (coords) => {
-    for (let i = 0)
-  }
+  componentDidMount() {}
+
   render() {
-    return (
-      <div className="Map">
-        <Room />
-      </div>
-    );
+    let mapObject = JSON.parse(localStorage.getItem("map"));
+    let mapRooms = [];
+    for (let roomId in mapObject) {
+      let coordinates = mapObject[roomId]["coordinates"];
+
+      coordinates = [coordinates[0] - 45, 30 - (coordinates[1] - 55)];
+
+      // let exits = mapObject[roomId]["coordinates"][1]; // will get to this later
+
+      // style += `left: ${coordinates[0] * 30}px; top:${coordinates[1] * 30}px;`; // apply to styled components
+      mapRooms.push(
+        <Room
+          key={mapObject[roomId]}
+          className={mapObject[roomId]}
+          inputLeft={`${coordinates[0] * 30}px`}
+          inputTop={`${coordinates[1] * 30}px`}
+        />
+      );
+    }
+    return <React.Fragment>{mapRooms}</React.Fragment>;
   }
 }
 
